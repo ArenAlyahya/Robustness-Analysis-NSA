dce62340beae76de17dab46702d1d789630c00e7
anooshafayyaz <anooshafayyaz333@gmail.com>
2023-11-02 11:37:41 -0400

    python3 version

--- run_everything.sh
+++ run_everything.sh
@@ -1,18 +1,18 @@
 cd input_data
-python generate_BR_network.py
-python generate_SP_network.py
-python generate_states_network.py
+python3 generate_BR_network.py
+python3 generate_SP_network.py
+python3 generate_states_network.py
 
 cd ../src/metrics
-python net_stats.py
+python3 net_stats.py
 
 cd ../sort_nodes
-python sort_nodes_according_to_covid.py
+python3 sort_nodes_according_to_covid.py
 
 cd ../robustness
-python network_robustness_failure.py
-python network_robustness_stats.py
-python network_robustness_sorted_covid.py
+python3 network_robustness_failure.py
+python3 network_robustness_stats.py
+python3 network_robustness_sorted_covid.py
 
 cd ../plot
-python plot_R.py
+python3 plot_R.py
